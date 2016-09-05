859527c06635391bea13048f56b1ce2d9905dcb1
Christopher Macklen <cmacklen@gmail.com>
2016-09-05 00:16:34 -0600

    Reaction flux is now working
    Note: the RMS error between the newly calculated J and COMSOL J is two
    orders of magnitude worse than the equivalent MATLAB solution.

--- coslib/ttp.py
+++ coslib/ttp.py
@@ -1,17 +1,37 @@
 """Standard modules"""
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
 import ldp
+import matplotlib.pyplot as plt
 
 
-def get_param(filename, time, location=None, delta_t=0.1, delete=None):
+class SimMesh(object):
+    def __init__(self, mesh, neg, sep, pos):
+        self.mesh = mesh
+        self.neg = neg
+        self.pos = pos
+        self.sep = sep
+
+
+class SimData(object):
+    def __init__(self, ce, cse, phie, phis, j):
+        self.ce = ce
+        self.cse = cse
+        self.phie = phie
+        self.phis = phis
+        self.j = j
+
+    def get_sim_data(self, time_index, location):
+        return SimData(
+                self.ce[time_index, location], self.cse[time_index, location],
+                self.phie[time_index, location], self.phis[time_index, location],
+                self.j[time_index, location])
+
+def get_var(parameter, time, location=None, delta_t=0.1, delete=None):
     """Fetch parameter data from a given location and time"""
-    sheet = ldp.read_csv(filename, start=9, assume=ldp.NUMBER)
-    parameter = ldp.load_section(sheet)
     (x_parameter, y_parameter) = (parameter[:, 0], parameter[:, 1])
     time_frame = np.nonzero(np.diff(x_parameter) < 0)[0]
-    start = np.insert(time_frame+1, 0, 1)
+    start = np.insert(time_frame+1, 0, 0)
     stop = np.append(time_frame, len(x_parameter))
     time_range = np.arange(0, len(start))*delta_t
     time_index = np.nonzero(time_range == time)[0][0]
@@ -23,43 +43,106 @@ def get_param(filename, time, location=None, delta_t=0.1, delete=None):
     if delete:
         data = np.delete(data, delete)
 
-    return data
+    return np.array([data])
 
-def j(ce,cse,phie,phis,params,const):
-    nice_abs = lambda x: ((np.sign(x)+1)/2)*np.abs(x)
 
-    j0 = lambda ce, cse: params['k_norm_ref']*nice_abs((params['csmax']-cse)/params['csmax'])**(1-params['alpha'])*nice_abs(cse/params['csmax'])**params['alpha']*nice_abs(ce/const['ce0'])**(1-params['alpha'])
+def nice_abs(number):
+    """Return the absolute of the given number"""
+    return ((np.sign(number)+1)/2)*np.abs(number)
 
-    soc = lambda cse: cse/params['csmax']
-    eta = lambda cse, phie, phis: phis-phie-params['eref'](soc(cse))
+
+def reaction_flux(sim_data, params, const):
+    """J"""
+
+    reaction_flux0 = params['k_norm_ref'] * \
+        nice_abs((params['csmax']-sim_data.cse)/params['csmax']) ** \
+        (1-params['alpha']) * \
+        nice_abs(sim_data.cse/params['csmax']) ** params['alpha'] * \
+        nice_abs(sim_data.ce/const['ce0']) ** (1-params['alpha'])
+
+    soc = sim_data.cse/params['csmax']
+    # eta = phis-phie-params['eref'](soc)
+    eta = sim_data.phis-sim_data.phie-params['Uocp'][0](soc)
     F = 96487
     R = 8.314
-    return j0(ce,cse)*(np.exp((1-params['alpha'])*F*eta(cse, phie, phis)/(R*const['Tref']))-np.exp(-params['alpha']*F*eta(cse,phie,phis)/(R*const['Tref'])))
+    return np.array([reaction_flux0*(
+        np.exp((1-params['alpha'])*F*eta/(R*const['Tref'])) -
+        np.exp(-params['alpha']*F*eta/(R*const['Tref'])))])
+
+
+def region(mesh):
+    """Find the regions in the mesh"""
+    xneg = np.nonzero(mesh <= 1)[0]
+    xpos = np.nonzero(mesh > 2)[0]
+    xsep = np.nonzero((mesh > 1) & (mesh <= 2))[0]
+
+    if mesh[xneg[-1]] == mesh[xneg[-2]]:
+        xsep = np.concatenate((1, xneg[-1], xsep))
+        xneg = np.delete(xneg, -1)
+
+    if mesh[xsep[-1]] == mesh[xsep[-2]]:
+        xpos = np.concatenate((1, xsep[-1], xpos))
+        xsep = np.delete(xsep, -1)
+
+    return SimMesh(mesh, xneg, xsep, xpos)
+
+
+def assemble_comsol(time, data, space=None, dt=0.1):
+    ce, cse, phie, phis, j = (np.empty((0, len(data['mesh']))) for i in range(5))
+    for ind in time:
+        ce = np.append(ce, get_var(data['ce'], ind), axis=0)
+        cse = np.append(cse, get_var(data['cse'], ind, delete=[80, 202]), axis=0)
+        phie = np.append(phie, get_var(data['phie'], ind), axis=0)
+        phis = np.append(phis, get_var(data['phis'], ind, delete=[80, 202]), axis=0)
+        j = np.append(j, get_var(data['j'], ind, delete=[80, 202]), axis=0)
+
+    return SimData(ce, cse, phie, phis, j)
+
+
+def plot_j(time, data, mesh, params):
+    jneg = np.empty((0, len(mesh.neg)))
+    jpos = np.empty((0, len(mesh.pos)))
+
+    for ind in range(0,len(time)):
+        jneg = np.append(jneg, reaction_flux(data.get_sim_data(ind, mesh.neg), params['neg'], params['const']), axis=0)
+        jpos = np.append(jpos, reaction_flux(data.get_sim_data(ind, mesh.pos), params['pos'], params['const']), axis=0)
+        plt.plot(mesh.neg, jneg[ind,:], mesh.pos, jpos[ind,:])
+
+    print('Neg rms: {}'.format(np.sqrt(np.mean(np.square(jneg-data.get_sim_data(slice(0,len(time)), mesh.neg).j), axis=1))))
+    print('Pos rms: {}'.format(np.sqrt(np.mean(np.square(jpos-data.get_sim_data(slice(0,len(time)), mesh.pos).j), axis=1))))
+    plt.grid()
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    plt.show()
+
 
 def main():
+    print('Loading Cell Parameters')
     params = dict()
-    sheet = ldp.read_excel('../tests/gold_standard/GuAndWang_parameter_list.xlsx', 0)
-    (ncol, pcol) = (2,3)
+    time = [5, 15, 25, 35, 45]
+    sheet = ldp.read_excel(
+        '../tests/gold_standard/GuAndWang_parameter_list.xlsx', 0)
+    (ncol, pcol) = (2, 3)
     params['const'] = ldp.load_params(sheet, range(7, 15), ncol, pcol)
     params['neg'] = ldp.load_params(sheet, range(18, 43), ncol, pcol)
     params['sep'] = ldp.load_params(sheet, range(47, 52), ncol, pcol)
     params['pos'] = ldp.load_params(sheet, range(55, 75), ncol, pcol)
 
-    eref_neg = ldp.loadtxt('../tests/gold_standard/eref_neg.csv', comments='%', delimiter=',')
-    sheet = ldp.read_csv('../tests/gold_standard/eref_neg.csv', assume=ldp.NUMBER)
-    eref_neg = ldp.load_section(sheet)
-    sheet = ldp.read_csv('../tests/gold_standard/eref_pos.csv', assume=ldp.NUMBER)
-    eref_pos = ldp.load_section(sheet)
+    comsol = ldp.load('../tests/gold_standard/guwang2.npz')
 
-    params['neg']['eref'] = lambda soc: np.interp(soc, eref_neg[:][0], eref_neg[:][1])
-    params['pos']['eref'] = lambda soc: np.interp(soc, eref_pos[:][0], eref_pos[:][1])
+    comsol_parsed = assemble_comsol(time, comsol)
 
-    ce = get_param('../tests/gold_standard/ce.csv', 5)
-    cse = get_param('../tests/gold_standard/cse.csv', 5, delete=[80,202])
-    phie = get_param('../tests/gold_standard/phie.csv', 5)
-    phis = get_param('../tests/gold_standard/phis.csv', 5, delete=[80,202])
+    comsol_mesh = region(comsol['mesh'])
+    plot_j(time, comsol_parsed, comsol_mesh, params)
 
-    print(j(ce,cse,phie,phis,params['neg'],params['const']))
+    return
+    ce = get_var(comsol['ce'], 5)
+    cse = get_var(comsol['cse'], 5, delete=[80, 202])
+    phie = get_var(comsol['phie'], 5)
+    phis = get_var(comsol['phis'], 5, delete=[80, 202])
+    mesh_neg, mesh_sep, mesh_pos = region(comsol['mesh'])
+    print(mesh_neg)
+
+    print(reaction_flux(ce, cse, phie, phis, params['neg'], params['const']))
 
 if __name__ == '__main__':
     sys.exit(main())
